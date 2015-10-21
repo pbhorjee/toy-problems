f48ec4e913d7900d22a83bddaa11c341b750def2
pbhorjee <paul@bhorjee.com>
2015-10-21 12:50:45 -0500

    even better 3largest

--- largestProductOfThree/largestProductOfThree.js
+++ largestProductOfThree/largestProductOfThree.js
@@ -9,29 +9,7 @@
 
 
 var largestProductOfThree = function(array) {
-  var largest3 = [array[0]];
-
-  for (var i = 1; i < array.length; i++) {
-    var toPush = undefined;
-
-    largest3.sort();
-
-    for (var l = 0; l < largest3.length; l++) {
-      if (largest3.length < 3) {
-        toPush = array[i];
-        break;
-      } else if (array[i] > largest3[l]) {
-        largest3[l] = array[i];
-        break;
-      }
-    };
-
-    if (toPush) { 
-      largest3.push(toPush); 
-    }
-  };
-
-  return largest3.reduce(function(a,b) {
+  return array.sort().slice(array.length-3).reduce(function(a,b) {
     return a * b;
   });
 };
