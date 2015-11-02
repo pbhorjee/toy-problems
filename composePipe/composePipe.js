7f92bb234bd4dd58e7619edc519199bc493031c7
pbhorjee <paul@bhorjee.com>
2015-11-02 10:24:36 -0600

    Testing webstorm

--- composePipe/composePipe.js
+++ composePipe/composePipe.js
@@ -1,7 +1,7 @@
 /*
  * Write Compose and Pipe functions.
- * 
- * Step 1: Implement the function Compose: 
+ *
+ * Step 1: Implement the function Compose:
  *
  * Compose should return a function that is the composition of a list of
  * functions of arbitrary length.
@@ -19,11 +19,11 @@
  * Step 2: Implement the function Pipe:
  *
  * Pipe composes a series of functions and returns the resulting function.
- * 
+ *
  * Each function is called on the return value of the preceding function.
  *
  * You can view pipe as moving left to right through its arguments.
- * 
+ *
  * Pipe Example:
  *  var add2 = function(number){ return number + 2; }
  *  var multiplyBy3 = function(number){ return number * 3; }
@@ -33,49 +33,33 @@
 
 'use strict';
 
+var compose = function () {
 
-var pipe = function() {
+  var args = Array.prototype.slice.call(arguments);
 
-  for (var i = 0; i < arguments.length; i++) {
-    arguments[i];
-  }
+  return function (val) {
+    return args.reduceRight(function (memo, fn) {
+      return fn(memo);
+    }, val);
+  };
+};
 
-}
+var pipe = function () {
+  var args = Array.prototype.slice.call(arguments);
 
-var add2 = function(number) {
-  return number + 2; 
-}
+  return function (val) {
+    return args.reduce(function (memo, fn) {
+      return fn(memo);
+    }, val);
+  };
+};
 
-var multiplyBy3 = function(number) { 
+var add2 = function (number) {
+  return number + 2;
+};
+
+var multiplyBy3 = function (number) {
   return number * 3;
-}
+};
 
-//console.log(pipe);
 console.log(pipe(add2, multiplyBy3)(5)); // 21
-
-
-// pipe(add2, multiplyBy3, multiplyBy3)(5) // 63
-
-
-
-
-// var compose = function() {
-//   var ret;
-
-//   for (var i = arguments.length - 1; i >= 1; i--) {
-//     ret += arguments[i](ret);
-//   };
-
-//   return ret;
-// };
-
-// var greet = function(name) { 
-//   //return 'hi: ' + name;
-// }
-// var exclaim = function(statement) { 
-//   //return statement.toUpperCase() + '!';
-// } 
-
-// var welcome = compose(greet, exclaim);
-
-// console.log(welcome('phillip')); // 'hi: PHILLIP!'
