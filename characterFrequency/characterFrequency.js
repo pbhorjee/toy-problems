4a95ef32195b315504220f088939907778934ac0
Paul Bhorjee <paul@bhorjee.com>
2016-02-02 10:26:40 -0600

    fixed characterFrequency

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -1,6 +1,5 @@
 //Write a function that takes as its input a string and returns an array of arrays as shown below sorted in descending order by frequency and then by ascending order by character.
 
-
 function characterFrequency(string) {
   var charsObj = {};
   var charsArr = [];
@@ -14,41 +13,28 @@ function characterFrequency(string) {
   }
 
   for (var char in charsObj) {
-    if (charsObj.hasOwnProperty(char)) {
-      var charArr = [char, charsObj[char]];
-
-      if (!charsArr.length) {
-        charsArr.push(charArr);
-      } else {
-        var posFound = false;
-        for (var i = 0; i < charsArr.length; i++) {
-          if (charArr[1] >= charsArr[i][1]) {
-
-            if (charsArr[i + 1]) {
-              //TODO: The innder test to see if we're at the right place as far the chars go isn't working
-              if (charArr[1] >= charsArr[i + 1][1] && charArr[0] < charsArr[i + 1][0]) {
-                charsArr.splice(i, 0, charArr);
-                posFound = true;
-                break;
-              }
-            } else {
-              charsArr.splice(i, 0, charArr);
-              posFound = true;
-              break;
-            }
-          }
-        }
-
-        if (!posFound) {
-          charsArr.push(charArr);
-        }
-      }
-    }
+    charsArr.push([char, charsObj[char]]);
   }
 
-  return charsArr;
+  function compare(a, b) {
+    if (a[1] < b[1]) {
+      return 1;
+    }
+    if (a[1] > b[1]) {
+      return -1;
+    }
+    if (a[0] > b[0]) {
+      return 1;
+    }
+    if (a[0] < b[0]) {
+      return -1;
+    }
+    return 0;
+  }
+
+  return charsArr.sort(compare);
 }
 
-//console.log(characterFrequency("aaabbc"));
-//console.log(characterFrequency("mississippi"));
-console.log((characterFrequency("supercalifragilisticexpialidocious")));
+console.log(characterFrequency("aaabbc"));
+console.log(characterFrequency("mississippi"));
+console.log(characterFrequency("supercalifragilisticexpialidocious"));
